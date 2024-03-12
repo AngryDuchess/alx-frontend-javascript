212c12f04a408fca4ed1fd06b9bb4d32f6bc17c2
Mahama Hamida <mahamahamida@gmail.com>
2024-03-12 23:45:30 +0000

    Refactor handleProfileSignup to use async/await

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,6 +1,6 @@
 import { uploadPhoto, createUser } from "./utils";
 
-function handleProfileSignup() {
+async function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
     .then((values) => {
       console.log(values[0].body, values[1].firstName, values[1].lastName);
