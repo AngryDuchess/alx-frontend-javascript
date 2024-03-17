34912b28a4f6cbf24f1dd3092596b4a8a732bd5c
Mahama Hamida <mahamahamida@gmail.com>
2024-03-17 13:15:53 +0000

    Refactor SkyHighBuilding constructor

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -2,13 +2,13 @@ import Building from './5-building';
 
 /**
  * Inherits from Building class
- * 
+ *
  */
 export default class SkyHighBuilding extends Building {
-    /**
-     * @param {number} sqft 
-     * @param {number} floors 
-     */
+  /**
+   * @param {number} sqft
+   * @param {number} floors
+   */
   constructor(sqft, floors) {
     super(sqft);
     this._floors = floors;
