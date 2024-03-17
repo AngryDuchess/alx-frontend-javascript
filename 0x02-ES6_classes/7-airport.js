dae4a0c35f67331aec7d1627e1d8d97aacbbf362
Mahama Hamida <mahamahamida@gmail.com>
2024-03-17 13:13:08 +0000

    Add Airport class implementation

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,13 @@
+/**
+ * A class to represent an airport
+ */
+export default class Airport {
+  constructor(name, code) {
+    this._name = name;
+    this._code = code;
+  }
+
+  toString() {
+    return `[object ${this._code}]`;
+  }
+}
